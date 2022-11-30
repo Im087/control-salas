518838cba886a6a5fba9e8505c5f11c593d6e79c
D.Lin <lindaqi087@gmail.com>
2022-12-01 00:36:44 +0100

    update floor component

--- src/components/FloorComp/FloorComp.vue
+++ src/components/FloorComp/FloorComp.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    floor
+  <div class="floor-comp p-4">
+    <div class="d-flex justify-content-between mb-3">
+      <h2 class="floor-comp_title">Planta 1</h2>
+      <button class="floor-comp_add-button btn btn-lg px-5 py-3" type="button">Añadir sala</button>
+    </div>
     <Room-comp></Room-comp>
   </div>
 </template>
@@ -23,6 +26,21 @@ export default defineComponent({
 });
 </script>
 
-<style>
+<style lang="scss" scoped>
+.floor-comp {
+  border: #2e344d 1px solid;
+  border-radius: 26px;
 
+  &_title {
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #2e344d;
+  }
+
+  &_add-button {
+    background-color: #2e344d;
+    color: #fff;
+    border-radius: 12px;
+  }
+}
 </style>
